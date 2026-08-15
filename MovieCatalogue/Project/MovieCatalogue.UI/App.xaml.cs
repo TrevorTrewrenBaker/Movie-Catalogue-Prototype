1deb5e003cc6c10ebb87fb5c5bf5a0c14687b5bc
Trevor Baker <trevor-baker@live.com.au>
2026-08-15 12:34:21 +1000

    TASK-01: Setup dependency injection frameworks for implementing the code base

--- MovieCatalogue/Project/MovieCatalogue.UI/App.xaml.cs
+++ MovieCatalogue/Project/MovieCatalogue.UI/App.xaml.cs
@@ -1,14 +1,30 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using Microsoft.Extensions.DependencyInjection;
+using MovieCatalogue.Application;
+using MovieCatalogue.Infrastructure;
 using System.Windows;
 
 namespace MovieCatalogue.UI
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    public partial class App : System.Windows.Application
     {
-    }
+        public IServiceProvider Services { get; private set; } = null!;
 
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var services = new ServiceCollection();
+
+            services.AddApplication();
+            services.AddInfrastructure();
+            services.AddViewModels();
+            services.AddViews();
+
+            Services = services.BuildServiceProvider();
+
+            var mainWindow = Services.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+
+            base.OnStartup(e);
+        }
+
+    }
 }
---
+++ MovieCatalogue/Project/MovieCatalogue.UI/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MovieCatalogue.UI;
+
+namespace MovieCatalogue.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddViewModels(this IServiceCollection services)
+        {
+            //services.AddTransient<MainViewModel>();
+            return services;
+        }
+
+        public static IServiceCollection AddViews(this IServiceCollection services)
+        {
+            services.AddTransient<MainWindow>();
+            return services;
+        }
+    }
+}
